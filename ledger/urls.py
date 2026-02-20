53d2468538ec9d1e371283799e7c001d2f733fc8
Raphaelle Abby Montayre <raphaelle.abby.montayre@student.ateneo.edu>
2026-02-21 02:06:48 +0800

    adjusted views, urls, and recipe calls

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,8 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('recipes/list/', recipe_list, name='recipe_list'),
-    path('recipe/1/', recipe_1, name='recipe_1'),
-    path('recipe/2/', recipe_2, name='recipe_2'),
+    path('recipe/<str:name>/', recipe, name='recipe'),
 ]
 
 app_name = "ledger"
