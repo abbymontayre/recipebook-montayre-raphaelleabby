cba73735334c05b49137acd124113fa5b70c7e42
Raphaelle Abby Montayre <raphaelle.abby.montayre@student.ateneo.edu>
2026-02-21 02:05:38 +0800

    made Ingredient, Recipe, RecipeIngredient models and made initial migration

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from datetime import datetime
+from django.urls import *
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ledger:ingredient', args=[str(self.name)])
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ledger:recipe', args=[str(self.name)])
+    
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=50)
+    ingredient = models.ForeignKey(
+            Ingredient, on_delete=models.CASCADE, related_name="recipe"
+        )
+    name = models.CharField(max_length=255)
+    recipe = models.ForeignKey(
+            Recipe, on_delete=models.CASCADE, related_name="ingredients"
+        )
+    
+    def __str__(self):
+        return f"{self.ingredient} - {self.quantity}"
+    
