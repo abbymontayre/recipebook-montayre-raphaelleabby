a84e385673687c1107cfa41520caa959fe12f0e6
Raphaelle Abby Montayre <raphaelle.abby.montayre@student.ateneo.edu>
2026-02-21 10:17:47 +0800

    [bonus] implemented recipe admin panel with recipeingredient inline admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
 
-# Register your models here.
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    fields = ("ingredient", "quantity")
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    inlines = [RecipeIngredientInline]
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
